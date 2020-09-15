e24ee3d16e3fc03f74c537103c45e47a87e33f2a
flamincome <flamincome@protonmail.com>
2020-09-15 13:25:57 +0800

    dev

---
+++ instances/StrategyBaselineGlucoseUSDC.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "../implementations/strategy/StrategyBaselineGlucose.sol";
+
+contract StrategyBaselineGlucoseUSDC is StrategyBaselineGlucose {
+    constructor()
+        public
+        StrategyBaselineGlucose(
+            address(0x6B175474E89094C44Da98b954EedeAC495271d0F),
+            address(0xDc03b4900Eff97d997f4B828ae0a45cd48C3b22d)
+        )
+    {}
+}
