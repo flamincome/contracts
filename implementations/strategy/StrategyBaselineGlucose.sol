ca0ecf55cee99d116618418db6d6859202f562e1
flamincome <flamincome@protonmail.com>
2020-09-15 12:01:32 +0800

    dev

---
+++ implementations/strategy/StrategyBaselineGlucose.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+import "../../interfaces/flamincome/Controller.sol";
+import "../../interfaces/flamincome/Vault.sol";
+
+import "./StrategyBaseline.sol";
+
+contract StrategyBaselineGlucose is StrategyBaseline {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    uint256 public feen = 5e15;
+    uint256 public constant feed = 1e18;
+
+    constructor(address _want, address _controller)
+        public
+        StrategyBaseline(_want, _controller)
+    {}
+
+    function withdraw(uint256 _amount) external override {
+        require(msg.sender == controller, "!controller");
+        uint256 _balance = IERC20(want).balanceOf(address(this));
+        if (_balance < _amount) {
+            _amount = _balance;
+        }
+        uint256 _fee = _amount.mul(feen).div(feed);
+        IERC20(want).safeTransfer(Controller(controller).rewards(), _fee);
+        address _vault = Controller(controller).vaults(address(want));
+        require(_vault != address(0), "!vault");
+        IERC20(want).safeTransfer(_vault, _amount.sub(_fee));
+    }
+
+    function setFeeN(uint256 _feen) external {
+        require(msg.sender == governance, "!governance");
+        feen = _feen;
+    }
+}
