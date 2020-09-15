47a875245c91f32cfc48d9f621ae399632cb86f4
flamincome <flamincome@protonmail.com>
2020-09-15 21:33:44 +0800

    dev

--- implementations/strategy/StrategyBaselineBenzeneYearnYDAI.sol
+++ implementations/strategy/StrategyBaselineBenzeneYearnYDAI.sol
@@ -36,39 +36,15 @@ contract StrategyBaselineBenzeneYearnYDAI is StrategyBaselineBenzene {
         IYFIVault(y).deposit(_amount);
     }
 
-    // function WithdrawToken(uint256 _amount) public override {
-    //     IERC20(recv).safeApprove(curve, 0);
-    //     IERC20(recv).safeApprove(curve, _amount);
-    //     uint256[4] memory vec = [
-    //         uint256(0),
-    //         uint256(0),
-    //         uint256(0),
-    //         uint256(0)
-    //     ];
-    //     ICurveFi(curve).remove_liquidity(_amount, vec);
+    function WithdrawToken(uint256 _amount) public override {
+        IYFIVault(y).withdraw(_amount);
+    }
 
-    //     for (int128 i = 0; i < 4; i++) {
-    //         if (i == index) {
-    //             continue;
-    //         }
-    //         address erc20 = ICurveFi(curve).coins(i);
-    //         uint256 _bal = IERC20(erc20).balanceOf(address(this));
-    //         if (_bal > 0) {
-    //             IERC20(erc20).safeApprove(curve, 0);
-    //             IERC20(erc20).safeApprove(curve, _bal);
-    //             ICurveFi(curve).exchange(i, index, _bal, 0);
-    //         }
-    //     }
-    // }
+    function GetRecv() public override view returns (address) {
+        return address(0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8);
+    }
 
-    // function GetRecv() public override view returns (address) {
-    //     return address(0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8);
-    // }
-
-    // function GetPriceE18OfRecvInWant() public override view returns (uint256) {
-    //     return
-    //         ICurveFi(curve).get_virtual_price().mul(1e18).div(
-    //             IYFIVault(want).getPricePerFullShare()
-    //         );
-    // }
+    function GetPriceE18OfRecvInWant() public override view returns (uint256) {
+        return IYFIVault(y).getPricePerFullShare();
+    }
 }
