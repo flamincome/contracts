ab32fc341f72397e5202566288dd610d88373c0e
flamincome <flamincome@protonmail.com>
2020-09-18 15:09:42 +0800

    dev

--- interfaces/external/Curve.sol
+++ interfaces/external/Curve.sol
@@ -10,3 +10,13 @@ interface ICurveFi {
   function exchange(int128 from, int128 to, uint256 _from_amount, uint256 _min_to_amount) external;
   function exchange_underlying(int128 from, int128 to, uint256 _from_amount, uint256 _min_to_amount) external;
 }
+
+interface ICurveFiBTC {
+  function coins(int128) external view returns (address);
+  function get_virtual_price() external view returns (uint);
+  function add_liquidity(uint256[3] calldata amounts, uint256 min_mint_amount) external;
+  function remove_liquidity_imbalance(uint256[3] calldata amounts, uint256 max_burn_amount) external;
+  function remove_liquidity(uint256 _amount, uint256[3] calldata amounts) external;
+  function exchange(int128 from, int128 to, uint256 _from_amount, uint256 _min_to_amount) external;
+  function exchange_underlying(int128 from, int128 to, uint256 _from_amount, uint256 _min_to_amount) external;
+}
--- interfaces/flamincome/Normalizer.sol
+++ interfaces/flamincome/Normalizer.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.6.2;
 interface Normalizer {
     function f(address) external view returns (uint256);
     function n(address) external view returns (uint256);
+    function GetMaximumNToken(address _addr) public view returns (uint256);
     function DepositFToken(uint) external;
     function WithdrawFToken(uint) external;
     function MintNToken(uint) external;
     function BurnNToken(uint) external;
+    function RealizeFToken(uint256 _d, uint256 _m) external;
+    function UnrealizeFToken(uint256 _w, uint256 _b) external;
 }
