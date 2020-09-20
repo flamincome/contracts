65bb9640440765793976376582d553fde57fbdf1
flamincome <flamincome@protonmail.com>
2020-09-20 23:47:55 +0800

    dev

---
+++ instances/StrategyBaselineBenzeneCurveBTCDAI.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "../implementations/strategy/StrategyBaselineBenzeneCurveBTC.sol";
+
+contract StrategyBaselineBenzeneCurveBTCDAI is StrategyBaselineBenzeneCurveBTC {
+    constructor()
+        public
+        StrategyBaselineBenzeneCurveBTC(
+            1,
+            address(0xDc03b4900Eff97d997f4B828ae0a45cd48C3b22d)
+        )
+    {}
+}
