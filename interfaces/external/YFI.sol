4096c9d1a4bc6935ab8df3b879c466ed10592541
flamincome <flamincome@protonmail.com>
2020-09-21 17:48:55 +0800

    dev

--- interfaces/external/YFI.sol
+++ interfaces/external/YFI.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.6.2;
 interface IYFIVault {
   function token() external view returns (address);
   function deposit(uint256 _amount) external;
+  function depositAll() external;
+  function withdrawAll() external;
   function withdraw(uint256 _amount) external;
   function getPricePerFullShare() external view returns (uint);
+  function earn() external;
 }
