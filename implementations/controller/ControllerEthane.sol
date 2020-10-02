b853f981e30a2694f2e154d47270a7ff47649e56
jihui.zheng <zjhsdtc@gmail.com>
2020-10-02 15:42:01 -0700

    feat: complete controller ethane

--- implementations/controller/ControllerEthane.sol
+++ implementations/controller/ControllerEthane.sol
@@ -49,88 +49,110 @@ contract ControllerEthane {
     }
 
     function AddStrategy(address _token, address _strategy) public {
-        // TODO: IMPL
-        // require(msg.sender == strategist || msg.sender == governance, "!strategist");
-        // address _current = strategies[_token];
-        // if (_current != address(0)) {
-        //    Strategy(_current).withdrawAll();
-        // }
-        // strategies[_token] = _strategy;
+        require(msg.sender == governance, "!governance");
+        strategies[_token].push(_strategy);
     }
 
     function RemoveStrategy(address _token, address _strategy) public {
-        // TODO: IMPL
-        // require(msg.sender == strategist || msg.sender == governance, "!strategist");
-        // address _current = strategies[_token];
-        // if (_current != address(0)) {
-        //    Strategy(_current).withdrawAll();
-        // }
-        // strategies[_token] = _strategy;
+        require(msg.sender == governance, "!governance");
+
+        address[] storage _tokenStrategies = strategies[_token];
+
+        for (uint i = 0; i < _tokenStrategies.length - 1; i++)
+            if (_tokenStrategies[i] == _strategy) {
+                _tokenStrategies[i] = _tokenStrategies[_tokenStrategies.length - 1];
+                break;
+            }
+        _tokenStrategies.pop();
+
+        Strategy(_strategy).withdrawAll();
     }
 
     function SetDefaultDepositStrategy(address _token, address _strategy)
         public
     {
-        // TODO: IMPL
-        // require(msg.sender == strategist || msg.sender == governance, "!strategist");
-        // address _current = strategies[_token];
-        // if (_current != address(0)) {
-        //    Strategy(_current).withdrawAll();
-        // }
-        // strategies[_token] = _strategy;
+        require(msg.sender == governance, "!governance");
+        default_deposit_strategy[_token] = _strategy;
     }
 
     function SetDefaultWithdrawStrategy(address _token, address _strategy)
         public
     {
-        // TODO: IMPL
-        // require(msg.sender == strategist || msg.sender == governance, "!strategist");
-        // address _current = strategies[_token];
-        // if (_current != address(0)) {
-        //    Strategy(_current).withdrawAll();
-        // }
-        // strategies[_token] = _strategy;
+        require(msg.sender == governance, "!governance");
+        default_withdraw_strategy[_token] = _strategy;
     }
 
     function DepositTokenByNE18(address _token, uint256 _ne18) public {
-        // TODO: IMPL
-        // address _strategy = strategies[_token];
-        // address _want = Strategy(_strategy).want();
-        // IERC20(_token).safeTransfer(_strategy, _amount);
-        // Strategy(_strategy).deposit();
+        require(msg.sender == strategist, "!strategist");
+        uint256 _depositAmount = IERC20(_token).balanceOf(address(this)).mul(_ne18).div(1e18);
+        DepositTokenByAmount(_token, _depositAmount);
     }
 
-    function DepositTokenByAmount(address _token, uint256 _amount) public {
-        // TODO: IMPL
-        // address _strategy = strategies[_token];
-        // address _want = Strategy(_strategy).want();
-        // IERC20(_token).safeTransfer(_strategy, _amount);
-        // Strategy(_strategy).deposit();
+    function DepositTokenByAmount(address _token, uint256 _amount) internal {
+        address _strategy = default_deposit_strategy[_token];
+        IERC20(_token).safeTransfer(_strategy, _amount);
+        Strategy(_strategy).deposit();
     }
 
     function WithdrawTokenByNE18(address _token, uint256 _ne18) public {
-        // TODO: IMPL
-        // address _strategy = strategies[_token];
-        // address _want = Strategy(_strategy).want();
-        // IERC20(_token).safeTransfer(_strategy, _amount);
-        // Strategy(_strategy).deposit();
+        require(msg.sender == strategist, "!strategist");
+        uint256 _withdrawAmount = GetBalance(_token).mul(_ne18).div(1e18);
+        WithdrawTokenByAmount(_token, _withdrawAmount);
     }
 
-    function WithdrawTokenByAmount(address _token, uint256 _ne18) public {
-        // TODO: IMPL
-        // address _strategy = strategies[_token];
-        // address _want = Strategy(_strategy).want();
-        // IERC20(_token).safeTransfer(_strategy, _amount);
-        // Strategy(_strategy).deposit();
+    function WithdrawChain(address _token, address _default_withdraw_strategy, uint256 _amount) internal {
+        address[] memory _tokenStrategies = strategies[_token];
+
+        for (uint i = 0; i < _tokenStrategies.length; i++) {
+            address _strategy = _tokenStrategies[i];
+            if (_strategy == _default_withdraw_strategy) continue;
+
+            uint256 _balance = Strategy(_strategy).balanceOf();
+            if (_balance >= _amount) {
+                Strategy(_strategy).withdraw(_amount);
+                break;
+            } else {
+                Strategy(_strategy).withdraw(_balance);
+                _amount = _amount.sub(_balance);
+            }
+        }
+    }
+
+    function WithdrawTokenByAmount(address _token, uint256 _amount) internal {
+        uint256 _remain = IERC20(_token).balanceOf(address(this));
+        if (_remain >= _amount) {
+            IERC20(_token).safeTransfer(vaults[_token], _amount);
+        } else {
+            IERC20(_token).safeTransfer(vaults[_token], _remain);
+            uint256 _left = _amount.sub(_remain);
+
+            address _strategy = default_withdraw_strategy[_token];
+            uint256 _balance = Strategy(_strategy).balanceOf();
+            if (_balance >= _left) {
+                Strategy(_strategy).withdraw(_left);
+            } else {
+                Strategy(_strategy).withdraw(_balance);
+                WithdrawChain(_token, _strategy, _left.sub(_balance));
+            }
+        }
     }
 
     function GetBalance(address _token) public view returns (uint256) {
-        // TODO: IMPL
-        // return Strategy(strategies[_token]).balanceOf();
+        address[] memory _tokenStrategies = strategies[_token];
+
+        uint256 _totalBalance = IERC20(_token).balanceOf(address(this));
+
+        for (uint i = 0; i < _tokenStrategies.length; i++) {
+            address _strategy = _tokenStrategies[i];
+            uint256 _balance = Strategy(_strategy).balanceOf();
+            _totalBalance = _totalBalance.add(_balance);
+        }
+        return _totalBalance;
     }
 
     function earn(address _token, uint256 _amount) public {
-        DepositToken(_token, 1e18);
+        require(msg.sender == vaults[_token], "!vault");
+        DepositTokenByAmount(_token, _amount);
     }
 
     function balanceOf(address _token) external view returns (uint256) {
@@ -138,6 +160,7 @@ contract ControllerEthane {
     }
 
     function withdraw(address _token, uint256 _amount) public {
+        require(msg.sender == vaults[_token], "!vault");
         WithdrawTokenByAmount(_token, _amount);
     }
 }
