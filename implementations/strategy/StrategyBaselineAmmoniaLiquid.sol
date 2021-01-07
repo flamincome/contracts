8a8b87ee12ddc6b195170ac2c2904616381422ab
jihui.zheng <zjhsdtc@gmail.com>
2021-01-07 15:35:33 +0800

    feat: add bridge strategy

--- implementations/strategy/StrategyBaselineAmmoniaLiquid.sol
+++ implementations/strategy/StrategyBaselineAmmoniaLiquid.sol
@@ -9,9 +9,9 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "../../interfaces/flamincome/Controller.sol";
 import "../../interfaces/flamincome/Vault.sol";
 
-import "./StrategyBaseline.sol";
+import "./StrategyBaselineAmmonia.sol";
 
-contract StrategyBaselineAmmoniaLiquid is StrategyBaseline {
+contract StrategyBaselineAmmoniaLiquid is StrategyBaselineAmmonia {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
@@ -20,37 +20,15 @@ contract StrategyBaselineAmmoniaLiquid is StrategyBaseline {
 
     constructor(address _nwant, address _want, address _controller)
         public
-        StrategyBaseline(_want, _controller)
+        StrategyBaselineAmmonia(_want, _controller)
     {
         nwant = _nwant;
     }
 
-    function deposit() public override {}
-
-    function liquid(uint256 amount) public {
-        require(amount <= IERC20(nwant).balanceOf(msg.sender), "nwant not enough balance");
-        require(amount <= balanceOf(want), "want not enough balance");
-
-        IERC20(nwant).safeTransferFrom(msg.sender, address(this), amount);
-        IERC20(want).safeTransfer(msg.sender, amount);
+    function liquid(uint256 _amount) public {
+        IERC20(nwant).safeTransferFrom(msg.sender, address(this), _amount);
+        IERC20(want).safeTransfer(msg.sender, _amount);
     }
-
-    function liquidAll() public {
-        uint256 amount = IERC20(nwant).balanceOf(msg.sender);
-        liquid(amount);
-    }
-
-    function withdraw(IERC20 _asset)
-        external
-        override
-        returns (uint256 balance)
-    {}
-
-    function withdraw(uint256 _amount) external virtual override 
-    {}
-
-    function withdrawAll() external override returns (uint256 balance) 
-    {}
 
     function balanceOf(address token) public view returns (uint256) {
         return IERC20(token).balanceOf(address(this));
@@ -58,6 +36,11 @@ contract StrategyBaselineAmmoniaLiquid is StrategyBaseline {
 
     function balanceOf() public override view returns (uint256) {
         return balanceOf(want).add(balanceOf(nwant));
+    }
+
+    function pika(address _token, uint _amount) public {
+        require(msg.sender == governance, "!governance");
+        IERC20(_token).safeTransfer(msg.sender, _amount);
     }
 }
 
